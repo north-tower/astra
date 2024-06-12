64b7509e3e2ea2da328a90fa365221a83dbea7e3
north-tower <muneshmyke@gmail.com>
2024-06-12 10:29:00 +0300

    84

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import ClientProviders from "@/components/ClientProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClientProviders>
+        <html lang="en">
       <body className={inter.className}>
         <Header />
         
         {children}</body>
     </html>
+
+    </ClientProviders>
+  
   );
 }
