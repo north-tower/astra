bf95c0e6b49b5b5c3df36b81a1ed894253d0d6f9
north-tower <muneshmyke@gmail.com>
2024-06-14 13:33:47 +0300

    119

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -1,21 +1,80 @@
-'use client'
+"use client"
 
+import React, { useState } from 'react'
 import { MessageSquarePlusIcon } from 'lucide-react'
-import React from 'react'
 import { Button } from './ui/button'
 import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { useToast } from './ui/use-toast'
+import { useSubscriptionStore } from '@/store/store'
+// import LoadingSpinner from './LoadingSpinner'
+import { serverTimestamp, setDoc } from 'firebase/firestore'
+import { addChatRef } from '@/lib/converters/ChatMembers'
+import { v4 as uuidv5} from 'uuid';
 
-function CreateChatButton() {
+function CreateChatButton({ isLarge}: { isLarge?: boolean}) {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  const subscription = useSubscriptionStore((state) => state.subscription)
 
-    const router = useRouter();
+    
+    
     const createNewChat = async () => {
+      if (!session?.user.id) return;
 
-        router.push('/chat/abc');
+      setLoading(true);
+      toast({
+        title: "Creating New Chat...",
+        description: "Hold tight while we create your new chat...",
+        duration: 3000,
+      });
+
+
+      const chatId = uuidv5()
+
+      await setDoc(addChatRef(chatId, session.user.id), {
+        userId: session.user.id!,
+        email: session.user.email!,
+        timestamp: serverTimestamp(),
+        isAdmin: true,
+        chatId: chatId,
+        image: session.user.image || "",
+      }).then(() => {
+        toast({
+          title: "Success",
+          description: "Your chat has been created",
+          className: "bg-green-600 text-white",
+          duration: 2000,
+        });
+        router.push(`/chat/${chatId}`);
+      }).catch(() => {
+        toast({
+          title: "Error",
+          description: "There was an error creating your chat",
+          variant: "destructive",
+        })
+      }).finally(() => {
+        setLoading(false)
+      })
+        
     }
+
+  if (isLarge)
+    return(
+      <div>
+        <Button variant={"default"} onClick={createNewChat}>
+          {loading ? "Loading..." : "Create a New Chat"}
+        </Button>
+      </div>
+  )
   return (
-    <Button variant="ghost" onClick={createNewChat} >
-        <MessageSquarePlusIcon  />
-    </Button>
+
+        <Button onClick={createNewChat} variant={"ghost"}>
+        <MessageSquarePlusIcon />
+        </Button>
+  
   )
 }
 
