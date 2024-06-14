1c13a744f240bb4d8b3ff4d3cdc3184de5a89d20
north-tower <muneshmyke@gmail.com>
2024-06-14 14:02:03 +0300

    121

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { authOptions } from '@/auth'
 import { getDocs } from 'firebase/firestore'
 import { getServerSession } from 'next-auth'
-// import { chatMembersCollectionGroupRef } from '@/lib/converters/ChatMembers';
-// import ChatListRows from './ChatListRows';
+import { chatMembersCollectionGroupRef } from '@/lib/converters/ChatMembers';
+import ChatListRows from './ChatListRows';
 
 async function ChatList() {
     const session = await getServerSession(authOptions);
 
-    // const chatsSnapshot = await getDocs(
-    //   chatMembersCollectionGroupRef(session?.user.id!)
-    // )
+    const chatsSnapshot = await getDocs(
+      chatMembersCollectionGroupRef(session?.user.id!)
+    )
 
-    // const initialChats = chatsSnapshot.docs.map((doc) => ({
-    //   ...doc.data(),
-    // }));
+    const initialChats = chatsSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      timestamp: null,
+    }));
 
   return (
-    <div>ChatList</div>
+    <ChatListRows initialChats={initialChats} />
   )
 }
 
---
+++ components/ChatListRows.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatListRows() {
+  return (
+    <div>ChatListRows</div>
+  )
+}
+
+export default ChatListRows
