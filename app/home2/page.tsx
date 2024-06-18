c3b431b7f4167b5cf844a3d48f5f8ac7bc627f2d
north-tower <muneshmyke@gmail.com>
2024-06-18 18:45:00 +0300

    Finito

--- app/home2/page.tsx
+++ app/home2/page.tsx
@@ -1,38 +1,38 @@
 import ChatPermissionError from '@/components/ChatPermissionError';
-import Feed from '@/components/Feed'
-import TinderCards from '@/components/TinderCards'
-import React from 'react'
+import Feed from '@/components/Feed';
+import TinderCards from '@/components/TinderCards';
+import React from 'react';
 
-type Props = { 
+type Props = {
   params: {};
   searchParams: {
     error: string;
   }
 }
 
-function page({ searchParams: {error} } : Props) {
+function Page({ searchParams: { error } }: Props) {
   return (
     <main>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-7xl mx-auto">
-   
-    
-    
-      <div className="hidden md:block">
-      <div>
-      {error && (
-        <div className='m-2'>
-          <ChatPermissionError />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-7xl mx-auto">
+        <div>
+          {error && (
+            <div className='m-2'>
+              <ChatPermissionError />
+            </div>
+          )}
+          <TinderCards />
+          {/* Feed component will be shown below TinderCards on mobile view */}
+          <div className="block md:hidden mt-4">
+            <Feed />
           </div>
-      )}
-    </div>
-        <Feed />
+        </div>
+        {/* Feed component will be shown on the left side on desktop view */}
+        <div className="hidden md:block">
+          <Feed />
+        </div>
       </div>
-      <div>
-        <TinderCards />
-      </div>
-    </div>
-  </main>
-  )
+    </main>
+  );
 }
 
-export default page
+export default Page;
